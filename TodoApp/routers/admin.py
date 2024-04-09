8b8510111299583e5b3f648fc0e66ccd16482c23
harishkumar-1509 <harishgandhi1509@gmail.com>
2024-04-09 16:06:37 +0530

    completed admin endpoint

--- TodoApp/routers/admin.py
+++ TodoApp/routers/admin.py
@@ -29,3 +29,13 @@ async def get_todos(user: user_dependency,
     if user is None or user.get('user_role') != 'admin':
         raise HTTPException(status_code=401, detail='Authentication Failed')
     return db.query(Todo).all()
+
+@router.delete('/todo/{todo_id}', status=status.HTTP_204_NO_CONTENT)
+async def delete_todo(user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
+    if user is None or user.get('user_role') != 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    todo_model = db.query(Todo).filter(Todo.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=400, detail='Todo not found')
+    db.query(Todo).filter(Todo.id == todo_id).delete()
+    db.commit()
